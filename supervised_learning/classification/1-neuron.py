58e62989f5836d5435da8992668edd41a50b87ea
mwaniasam <s.mwania@alustudent.com>
2026-01-28 07:32:56 +0530

    Privatize Neuron attributes with getter properties
    Convert public attributes to private (__W, __b, __A) and add
    @property decorators for read-only access

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Module for Neuron class"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron class for binary classification"""
+
+    def __init__(self, nx):
+        """
+        Initialize a Neuron instance
+
+        Args:
+            nx: number of input features to the neuron
+
+        Raises:
+            TypeError: if nx is not an integer
+            ValueError: if nx is less than 1
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for weights vector"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for activated output"""
+        return self.__A
