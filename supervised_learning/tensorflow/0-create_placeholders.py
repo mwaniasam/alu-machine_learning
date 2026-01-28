086af71ce6fe20bc40e534a7621e50457b6a332b
mwaniasam <s.mwania@alustudent.com>
2026-01-28 08:21:56 +0530

    Refactor create_placeholders function to improve documentation and clarity

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
-"""Creates a layer for a neural network"""
+'''
+    That returns two placeholders,
+    x and y, for the neural network
+'''
+
+
 import tensorflow as tf
 
 
-def create_layer(prev, n, activation):
-    """
-    Creates a layer for the neural network.
-
-    Args:
-        prev: tensor output of the previous layer
-        n: number of nodes in the layer
-        activation: activation function
-
-    Returns:
-        The tensor output of the layer
-    """
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.Dense(units=n, activation=activation,
-                            kernel_initializer=init, name='layer')
-    return layer(prev)
+def create_placeholders(nx, classes):
+    '''
+        that returns two placeholders,
+        x and y, for the neural network
+    '''
+    x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
+    y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
+    return x, y
