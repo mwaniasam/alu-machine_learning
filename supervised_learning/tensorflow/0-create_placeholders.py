b6046c4fd755e3ad193e37d3e067ee3af4f77ca9
mwaniasam <s.mwania@alustudent.com>
2026-01-28 08:27:23 +0530

    Refactor create_placeholders function to enhance documentation and clarity

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-'''
-    That returns two placeholders,
-    x and y, for the neural network
-'''
-
+"""Creates placeholders for a neural network"""
 
 import tensorflow as tf
 
 
 def create_placeholders(nx, classes):
-    '''
-        that returns two placeholders,
-        x and y, for the neural network
-    '''
+    """
+    Creates placeholders for input data and labels
+
+    Arguments:
+        nx (int): number of feature columns
+        classes (int): number of classes
+
+    Returns:
+        x (tf.placeholder): placeholder for input data
+        y (tf.placeholder): placeholder for one-hot labels
+    """
     x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
     y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
     return x, y
