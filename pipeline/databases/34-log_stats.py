5f911caf4d377dff130aecbf5ab5f1aebd161bae
mwaniasam <s.mwania@alustudent.com>
2026-02-28 19:45:16 +0530

    Optimize log count retrieval in log_stats module

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
-    total = collection.find().count()
+    total = collection.count()
     print("{} logs".format(total))
 
     print("Methods:")
