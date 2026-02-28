dd7fc48fff789e9fa96074d5eec8016f6b94f492
mwaniasam <s.mwania@alustudent.com>
2026-02-28 18:23:38 +0530

    A module to fetch home planets of sentient species from SWAPI API

---
+++ pipeline/apis/1-sentience.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Module for fetching home planets of sentient species from the SWAPI API"""
+import requests
+
+
+def sentientPlanets():
+    """Returns list of names of home planets of all sentient species"""
+    planets = []
+    url = 'https://swapi-api.hbtn.io/api/species/'
+
+    while url:
+        response = requests.get(url)
+        data = response.json()
+        for species in data['results']:
+            if (species['designation'] == 'sentient' or
+                    species['classification'] == 'sentient'):
+                homeworld = species['homeworld']
+                if homeworld:
+                    planet_response = requests.get(homeworld)
+                    planet_data = planet_response.json()
+                    planets.append(planet_data['name'])
+        url = data['next']
+
+    return planets
