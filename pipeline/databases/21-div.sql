0b9f9b61b9757e6dfe6f7d66cfbbb315d03dd0b8
mwaniasam <s.mwania@alustudent.com>
2026-02-28 19:22:57 +0530

    SafeDiv function to safely divide two integers

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,14 @@
+-- Creates a function SafeDiv that divides a by b or returns 0 if b is 0
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+    RETURN a / b;
+END$$
+
+DELIMITER ;
