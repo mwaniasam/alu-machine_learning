df38dbcc954bf6ffa6754cfaac2d059634315c1b
mwaniasam <s.mwania@alustudent.com>
2026-02-28 18:27:07 +0530

    Amodule to fetch upcoming SpaceX launches

---
+++ pipeline/apis/3-upcoming.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Module for fetching the upcoming SpaceX launch"""
+import requests
+
+
+if __name__ == '__main__':
+    url = 'https://api.spacexdata.com/v4/launches/upcoming'
+    response = requests.get(url)
+    launches = response.json()
+
+    upcoming = sorted(launches, key=lambda x: x['date_unix'])[0]
+
+    name = upcoming['name']
+    date = upcoming['date_local']
+
+    rocket_url = 'https://api.spacexdata.com/v4/rockets/{}'.format(
+        upcoming['rocket']
+    )
+    rocket = requests.get(rocket_url).json()
+    rocket_name = rocket['name']
+
+    pad_url = 'https://api.spacexdata.com/v4/launchpads/{}'.format(
+        upcoming['launchpad']
+    )
+    pad = requests.get(pad_url).json()
+    pad_name = pad['name']
+    pad_locality = pad['locality']
+
+    print('{} ({}) {} - {} ({})'.format(
+        name, date, rocket_name, pad_name, pad_locality
+    ))
