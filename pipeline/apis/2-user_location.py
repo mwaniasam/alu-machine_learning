ddf509ce45509496b2ea747dd174c559d90ff5a6
mwaniasam <s.mwania@alustudent.com>
2026-02-28 18:32:56 +0530

    Refactor user location fetching logic for improved error handling and clarity

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -7,21 +7,28 @@ import math
 
 
 if __name__ == '__main__':
-    url = sys.argv[1]
-    response = requests.get(url)
+    if len(sys.argv) < 2:
+        sys.exit(1)
 
-    if response.status_code == 404:
-        print('Not found')
-    elif response.status_code == 403:
-        reset_time = int(response.headers['X-Ratelimit-Reset'])
-        minutes = math.ceil((reset_time - int(time.time())) / 60)
-        print('Reset in {} min'.format(minutes))
-    elif response.status_code == 200:
-        data = response.json()
-        location = data.get('location')
-        if location:
-            print(location)
+    url = sys.argv[1]
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 403:
+            reset_time = int(response.headers.get('X-Ratelimit-Reset', 0))
+            minutes = math.ceil((reset_time - int(time.time())) / 60)
+            print('Reset in {} min'.format(minutes))
+        elif response.status_code == 404:
+            print('Not found')
+        elif response.status_code == 200:
+            data = response.json()
+            location = data.get('location')
+            if location is None or location == '':
+                print('Not found')
+            else:
+                print(location)
         else:
             print('Not found')
-    else:
+    except Exception:
         print('Not found')
