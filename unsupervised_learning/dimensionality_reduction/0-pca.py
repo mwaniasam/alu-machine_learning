af4b2775368d502abca0528d05b5d4a49121ccc6
Samuel Mwania <s.mwania@alustudent.com>
2026-07-01 11:42:55 +0300

    Performs PCA on a dataset

---
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Performs PCA on a dataset
+"""
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """
+    Performs PCA on a dataset
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+
+    U, S, Vh = np.linalg.svd(X)
+    cum_var = np.cumsum(S) / np.sum(S)
+
+    r = np.argwhere(cum_var >= var)[0, 0]
+
+    W = Vh[:r + 1].T
+
+    return W
